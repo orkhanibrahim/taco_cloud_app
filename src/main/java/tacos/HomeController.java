0dc5087cade1d2d38e4b0836a7a7c20b66345142
Orxan T. Ibrahim <IbrahimOT@kapitalho.az>
2021-05-27 00:02:53 +0400

    marked class as controller and added simple getMapping method

--- src/main/java/tacos/HomeController.java
+++ src/main/java/tacos/HomeController.java
@@ -1,5 +1,12 @@
 package tacos;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
 }

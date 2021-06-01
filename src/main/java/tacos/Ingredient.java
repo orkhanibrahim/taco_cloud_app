9b4014bce95475b70f60cee01ea126d3c1e7dc8f
Orxan T. Ibrahim <IbrahimOT@kapitalho.az>
2021-06-01 17:44:32 +0400

    Removed unnecessary lines and codes

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,24 +1,20 @@
 package tacos;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+
 @Data
-//@RequiredArgsConstructor
-//@AllArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+
 public class Ingredient {
-	
-public Ingredient(String id, String name, Type type) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.type = type;
-	}
 
-private  String id;
-private  String name;
-private  Type type;
+    private String id;
+    private String name;
+    private Type type;
 
-public static enum Type{
-	WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
-}
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
 }

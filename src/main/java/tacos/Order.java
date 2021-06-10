658428123a794db04df83b89b0e2a7b6db6c7109
Orxan T. Ibrahim <IbrahimOT@kapitalho.az>
2021-06-10 17:17:35 +0400

    Validated fields of Order.java class

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -10,13 +10,21 @@ import lombok.Data;
 
 @Data
 public class Order {
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "Street address is required")
     private String street;
+    @NotBlank(message = "City is required")
     private String city;
+    @NotBlank(message = "State is required")
     private String state;
+    @NotBlank(message = "Postal Code is required")
     private String zip;
+    @CreditCardNumber(message = "Not a valid credit number")
     private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\\\/])([1-9][0-9])$", message = "Must be formatted MM/YY")
     private String ccExpiration;
+    @Digits(integer = 3, fraction = 0, message = "Not valid cvv")
     private String ccCVV;
 
 }
